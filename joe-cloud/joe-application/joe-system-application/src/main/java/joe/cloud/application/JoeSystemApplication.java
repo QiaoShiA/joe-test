2a2ec61fc74812c077e45b5b6dd44ef28daff351
joe <1014295099@qq.com>
2022-07-13 18:00:00 +0800

    init

---
+++ joe-cloud/joe-application/joe-system-application/src/main/java/joe/cloud/application/JoeSystemApplication.java
@@ -0,0 +1,13 @@
+package joe.cloud.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoeSystemApplication.class, args);
+    }
+
+}
---
+++ joe-cloud/joe-application/joe-system-application/src/test/java/joe/cloud/application/JoeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package joe.cloud.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
